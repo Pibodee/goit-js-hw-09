d2f390af04237174ac7220ccc15c2db5ecc73ff4
Pibodee <mrbodyanuch@gmail.com>
2023-01-06 17:30:52 +0200

    task 1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const background = document.querySelector('body');
 
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+start.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        background.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    start.disabled = true;
+    stop.disabled = false;
+})
+
+stop.addEventListener('click', () => {
+    clearInterval(timerId);
+    start.disabled = false;
+    stop.disabled = true;
+})
