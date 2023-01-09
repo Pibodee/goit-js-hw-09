4f6696c2e8df9e345b5e63b961ef93b3729fcab2
Pibodee <mrbodyanuch@gmail.com>
2023-01-09 06:43:47 +0200

    added task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const delay = document.querySelector('input[name="delay"]')
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const create = document.querySelector('button[type = "submit"]')
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+const shouldResolve = Math.random() > 0.3;
+if (shouldResolve) {
+  res({position, delay})
+} else {
+  rej({ position, delay });
 }
+    }, delay)
+  })
+}
+
+create.addEventListener('click', evt => {
+  evt.preventDefault();
+  const firstDelay = Number(delay.value);
+  const nextDelay = Number(step.value);
+  for (let i = 0; i < amount.value; i ++){
+    createPromise(i + 1, firstDelay + i * nextDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+})
